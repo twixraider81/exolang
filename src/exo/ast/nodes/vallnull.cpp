b096207d05a728f5cfd0017e3ff8167f2f51a9c9
twixraider81@gmail.com <twixraider81@gmail.com>
2014-04-08 15:34:49 +0200

    add null value, it is wrong done because it should not be a constFalse

---
+++ src/exo/ast/nodes/vallnull.cpp
@@ -0,0 +1,32 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "exo/exo.h"
+#include "exo/ast/ast.h"
+
+namespace exo
+{
+	namespace ast
+	{
+		namespace nodes
+		{
+			llvm::Value* ValNull::Generate( exo::ast::Context* context )
+			{
+				TRACESECTION( "IR", "generating null" );
+				return( llvm::ConstantInt::getFalse( *(context->context) ) );
+			}
+		}
+	}
+}
